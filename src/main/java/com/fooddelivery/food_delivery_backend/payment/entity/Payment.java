acdeac03a4a96d609246c423597dfa7a2f9cc76e
AnishKadu-debug <anishofficial270@gmail.com>
2026-07-26 14:52:51 +0530

    add payment enums and entity

---
+++ src/main/java/com/fooddelivery/food_delivery_backend/payment/entity/Payment.java
@@ -0,0 +1,2 @@
+package com.fooddelivery.food_delivery_backend.payment.entity;public class Payment {
+}
---
+++ src/main/java/com/fooddelivery/food_delivery_backend/payment/enums/PaymentMethod.java
@@ -0,0 +1,2 @@
+package com.fooddelivery.food_delivery_backend.payment.enums;public class PaymentMethod {
+}
